c8cc4e2253a1d303c902fa738f7327166fd5b637
sophiau10 <47048538+sophiau10@users.noreply.github.com>
2019-02-19 18:25:00 -0800

    Uploaded Assignment 5

---
+++ Zain_Selena_Assignment5/Assets/CharacterScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharacterScript : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float rotationSpeed = 5;
+    public float smooth = 1f;
+    private Vector3 targetAngles;
+    public Text countText;
+    private int count=0;
+    public GameObject door;
+    public GameObject door2;
+    public ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCountText();
+    }
+    /*public ParticleSystem explosion;
+
+    public void Explode(Vector3 position)
+    {
+        Instantiate(explosion, position, Quartenion.identity);
+    }*/
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("We have hit " + other.name);
+        Instantiate(particles, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        count = count + 1;
+        SetCountText();
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //move and rotate
+        if (Input.GetKey(KeyCode.D))
+        {
+            targetAngles = transform.eulerAngles + 180f * Vector3.up;
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, Time.deltaTime);
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            //GetComponent<Animator>().SetBool("Moving", true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            targetAngles = transform.eulerAngles - 180f * Vector3.up;
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, Time.deltaTime);
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            //GetComponent<Animator>().SetBool("Moving", true);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            GetComponent<Animator>().SetBool("Moving", true);
+        }
+        //idle
+        if(!Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Animator>().SetBool("Moving", false);
+        }
+        //open door
+        if(count==5)
+        {
+            door.GetComponent<Animator>().SetBool("isOpen", true);
+        }
+        if (count == 10)
+        {
+            door2.GetComponent<Animator>().SetBool("isOpen", true);
+        }
+        if(count ==15)
+        {
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+}
---
+++ Zain_Selena_Assignment5/Assets/Script/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float rotationSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.right * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.left * rotationSpeed);
+        }
+    }
+}
